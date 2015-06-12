895976c5372b6fe68529d24592278c85f45e539b
mjames-upc <mjames@unidata.ucar.edu>
2015-06-12 11:57:06 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='ufpy',
+    version='',
+    packages=['ufpy','ufpy.dataaccess',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
