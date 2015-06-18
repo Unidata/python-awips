e773c89a3fe9e4d19709dcf4d3a1c5ab7e8b1fcb
mjames-upc <mjames@unidata.ucar.edu>
2015-06-18 10:45:58 -0600

    install reconfig

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='ufpy',
     version='',
-    packages=['ufpy','ufpy.dataaccess',],
+    #packages=['ufpy','ufpy.dataaccess',],
+    packages=find_packages(exclude='data'),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
