0e85dacbe2ebf89f4dabf07a05ea7aa4a96efcf6
mjames-upc <mjames@unidata.ucar.edu>
2015-06-18 10:25:33 -0600

    add dynamicserialize

---
+++ dynamicserialize/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='dynamicserialize',
+    version='',
+    packages=['dynamicserialize'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
