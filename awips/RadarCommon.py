215df60d859060541819e846f122ab161e2cab09
mjames-upc <mjames@ucar.edu>
2016-03-16 10:24:48 -0500

    removing superfluous code from RadarCommon

--- awips/RadarCommon.py
+++ awips/RadarCommon.py
@@ -29,6 +29,7 @@
 #    ------------    ----------    -----------    --------------------------
 #    08/13/2014      3393          nabowle        Initial creation to contain common
 #                                                 code for a2*radStub scripts.
+#    03/15/2015                    mjames@ucar    Edited/added to awips package as RadarCommon
 #
 #
 
@@ -40,93 +41,10 @@ from datetime import timedelta
 from awips import ThriftClient
 
 from dynamicserialize.dstypes.com.raytheon.uf.common.time import TimeRange
-from dynamicserialize.dstypes.com.raytheon.uf.common.dataplugin.level import Level
 from dynamicserialize.dstypes.com.raytheon.uf.common.dataplugin.radar.request import GetRadarDataRecordRequest
 
-def get_default_host():
-    from awips.dataaccess import DataAccessLayer
-    return DataAccessLayer.THRIFT_HOST
-
-
-def get_args_parser():
-    parser = argparse.ArgumentParser(conflict_handler="resolve")
-    parser.add_argument("--host", action="store", dest="host",
-                        help="EDEX server hostname",
-                        metavar="hostname")
-    parser.add_argument("--datetime", action="store", dest="datetime", 
-                    help="The start of the time range in YYYY-MM-DD HH:MM",
-                    metavar="datetime")
-    parser.add_argument("--radar", action="store", dest="radar", 
-                    help="The ICAO code for the radar",
-                    metavar="radar")
-    parser.add_argument("--code", action="store", dest="code", 
-                    help="The product code.", type=int,
-                    metavar="code")
-    parser.add_argument("--angle", action="store", dest="angle", default=0,
-                    help="The Elevation Angle", metavar="angle")
-    parser.add_argument("--description", action="store", dest="description", 
-                    help="The description.",
-                    metavar="desc")
-    parser.add_argument("--slop", action="store", dest="slop", default=60,
-                    help="The amount of slop, in seconds, to allow around the datetime.",
-                    metavar="slop",  type=int)
-    return parser
-
-
-def send_request(user_args):
-    slop = user_args.slop
-
-    dateTimeStr = user_args.datetime
-    if not dateTimeStr:
-        print >> sys.stderr, "DateTime not provided"
-        return
-    dateTime = datetime.strptime(dateTimeStr, "%Y-%m-%d %H:%M")
-    beginRange = dateTime - timedelta(0, slop)
-    endRange = dateTime + timedelta(0, slop)
-
-    timerange = TimeRange(beginRange, endRange)
-
-    radar = user_args.radar
-    if not radar:
-        print >> sys.stderr, "Radar code not provided"
-        return
-
-    code = user_args.code
-    if not code:
-        print >> sys.stderr, "Product code not provided"
-        return
-
-    angle = user_args.angle
-
-    slop = int(user_args.slop)
-
-    host = user_args.host
-    if not host:
-        host = get_default_host()
-
-    client = ThriftClient.ThriftClient(host)
-
-    # Perform a GetRadarHDF5Request
-    req = GetRadarDataRecordRequest()
-    req.setRadarId(radar)
-    req.setPrimaryElevationAngle(float(angle))
-    req.setTimeRange(timerange)
-    req.setProductCode(int(code))
-
-    response = client.sendRequest(req)
-
-    if response is None:
-        # print "Data not available"
-        return
-
-    records = response.getData()
-    return records
-
-
 def get_datetime_str(record):
-    #2014-07-16 00:00:00 (0) => 2014-07-16_00:03:00.0
     return str(record.getDataTime())[0:19].replace(" ","_") + ".0"
-
 
 def get_data_type(azdat):
     if azdat:
@@ -134,7 +52,6 @@ def get_data_type(azdat):
     else :
         dattyp = "raster"
     return dattyp
-
 
 def get_hdf5_data(idra):
     rdat = []
@@ -150,18 +67,6 @@ def get_hdf5_data(idra):
               dattyp = "radial"
            elif idra[ii].getName() == "DependentValues":
               depVals = idra[ii].getShortData()
-## Commented out from the original. May not be available.
-#          elif idra[ii].getName() == "ProductVals":
-#             prodVals = idra[ii].getByteData()
-#          elif idra[ii].getName() == "RecordVals":
-#             recVals = idra[ii].getByteData()
-#           elif idra[ii].getName() == "StormIds":
-#             stormVals = idra[ii].getByteData()
-#          elif idra[ii].getName() == "Symbology":
-#             symVals = idra[ii].getByteData()
-#          elif idra[ii].getName() == "SymbologyData":
-#             symData = idra[ii].getByteData()
-##
            elif idra[ii].getName() == "Thresholds":
               threshVals = idra[ii].getShortData()
 
@@ -170,7 +75,7 @@ def get_hdf5_data(idra):
 
 def get_header(record, format, xLen, yLen, azdat, description):
     # Encode dimensions, time, mapping, description, tilt, and VCP
-    mytime = get_datetime_str(record) 
+    mytime = get_datetime_str(record)
     dattyp = get_data_type(azdat)
 
     if format :
@@ -228,7 +133,7 @@ def encode_thresh_vals(threshVals):
     return msg
 
 
-def encode_dep_vals(depVals): 
+def encode_dep_vals(depVals):
     nnn = len(depVals)
     j = 0
     msg = []
@@ -246,4 +151,3 @@ def encode_radial(azVals):
         msg.append(azVals[j])
         j += 1
     return msg
-
