c8bf113cc77dc4dd2ba6b243fe8c23ce15175148
mjames-upc <mjames@unidata.ucar.edu>
2017-04-12 16:23:30 -0600

    setup.py updates for versioning, dependencies, py3 compliance.  closes #3

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
+# Copyright (c) 2017 UCAR Unidata Program Center.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+from __future__ import print_function
+import sys
 from distutils.core import setup
 from setuptools import find_packages
 
+dependencies = ['numpy','shapely','six']
+if sys.version_info < (3, 4):
+    dependencies.append('enum34')
+
+ver="0.9.10"
+
 setup(
     name='python-awips',
-    version='0.9.10',
+    version=ver,
     description='A framework for requesting AWIPS meteorological datasets from an EDEX server',
     packages=find_packages(exclude='data'),
-    license='Open Source',
+    license='BSD',
     url='http://python-awips.readthedocs.io',
-    download_url='https://github.com/Unidata/python-awips/tarball/0.9.10',
+    download_url='https://github.com/Unidata/python-awisp/archive/v{}.tar.gz'.format(ver),
     author='Unidata',
     author_email='mjames@ucar.edu',
-    requires=['argparse','shapely','numpy','six']
+    install_requires=dependencies,
+    extras_require={
+        'cdm': ['pyproj>=1.9.4'],
+        'dev': ['ipython[all]>=3.1'],
+        'doc': ['sphinx>=1.4', 'sphinx-gallery', 'doc8'],
+        'examples': ['cartopy>=0.13.1','metpy>=0.4.0']
+    }
 )
 
