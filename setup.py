425751dee4335460e9d99c4f0ab09d33bb852d7a
Shay Carter <srcarter@ucar.edu>
2022-01-05 13:22:52 -0700

    First pass at implementing automated publishing - created a new 'release.yml' file which is a GH Action that is triggered by a new Published Release - it builds python from the latest in the main branch, and then will push it to pip (that part is commented out for this first pass for testing)
    Small changes to the setup.py for python-awips builds
    - changed the doc url to point to our new/current doc site
    - changed the author's email from mj's to support-awips
    
    Add in logic for publishing release to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = ['numpy', 'six']
 if sys.version_info < (3, 4):
     dependencies.append('enum34')
 
-ver = "18.1.7"
+ver = "18.1.8"
 
 setup(
     name='python-awips',
@@ -18,10 +18,10 @@ setup(
     description='A framework for requesting AWIPS meteorological datasets from an EDEX server',
     packages=find_packages(exclude='data'),
     license='BSD',
-    url='http://python-awips.readthedocs.io',
+    url='http://unidata.github.io/python-awips',
     download_url='https://github.com/Unidata/python-awips/archive/{}.tar.gz'.format(ver),
     author='Unidata',
-    author_email='mjames@ucar.edu',
+    author_email='support-awips@ucar.edu',
     install_requires=dependencies,
     extras_require={
         'cdm': ['pyproj>=1.9.4'],
@@ -30,4 +30,3 @@ setup(
         'examples': ['cartopy>=0.13.1', 'metpy>=0.4.0']
     }
 )
-
