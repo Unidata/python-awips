2791429ca71f97987539bdf56e976115c57f0ab6
Shay Carter <srcarter@ucar.edu>
2023-12-12 11:45:16 -0700

    Small changes for new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = ['numpy']
 if sys.version_info < (3, 4):
     dependencies.append('enum34')
 
-ver = "20"
+ver = "20.1"
 
 setup(
     name='python-awips',
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(exclude='data'),
     license='BSD',
     url='http://unidata.github.io/python-awips',
-    author='Unidata',
+    author='NSF Unidata',
     author_email='support-awips@ucar.edu',
     install_requires=dependencies,
     extras_require={
